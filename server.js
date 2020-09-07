6854f11360d6bf88fcdf5038f6ea133cdc2c3b7f
Benjamin Krzis <47818681+DrBennemin@users.noreply.github.com>
2020-09-07 10:23:50 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
