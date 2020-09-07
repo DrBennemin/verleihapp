eee55029fdb57a65ba7110b70c525abe881dba90
DrBennemin <benjamin.krzis@gmail.com>
2020-09-07 15:30:09 +0200

    Add middleware

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const jsonServer = require("json-server");
-/* const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults(); */
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
-/* app.use(middlewares);
-app.use(router); */
+app.use(middlewares);
+app.use(router);
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
