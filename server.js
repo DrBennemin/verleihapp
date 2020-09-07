d81929468780e81fed6364a1430c53264a68d9c7
DrBennemin <benjamin.krzis@gmail.com>
2020-09-07 12:40:54 +0200

    Add path storybook and storybook static to gitignor

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 3000;
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
