c08989c83686cfbf1aa28e94433ace57c1866b12
DrBennemin <benjamin.krzis@gmail.com>
2020-09-21 14:11:15 +0200

    Changed design to strecht content to 90% vw

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "@emotion/styled";
 import Header from "../components/Header";
 import ListItem from "../components/ListItem";
 
@@ -6,9 +7,17 @@ function List() {
   return (
     <>
       <Header title={"Verleih-app"} />
-      <ListItem />
+      <Container>
+        <ListItem />
+      </Container>
     </>
   );
 }
 
 export default List;
+
+const Container = styled.section`
+  margin-bottom: 80px;
+  margin-top: 85px;
+  width: 90%;
+`;
