00c928e80c419e700a5cd3734d88788803ac2b5f
DrBennemin <benjamin.krzis@gmail.com>
2020-10-01 17:51:13 +0200

    Push build

--- client/src/components/OverlayModal.js
+++ client/src/components/OverlayModal.js
@@ -23,13 +23,35 @@ function OverlayModal({ onHandleDelete, onSetOverlay }) {
 const Container = styled.div`
   background-color: #fff;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
-  border-radius: 50px;
+  border-radius: 5px;
 `;
-const Header = styled.div``;
-const Main = styled.div``;
-const Footer = styled.div``;
+const Header = styled.h1`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 10px;
+  border-bottom: 1px solid #dedede;
+`;
+const Main = styled.div`
+  padding: 20px;
+  border-bottom: 1px solid #dedede;
+`;
+const Footer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+  & button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+  }
+`;
 
 export default OverlayModal;
