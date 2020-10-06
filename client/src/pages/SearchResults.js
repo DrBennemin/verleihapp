b781ad69d79205cd50977fae97c697f5055d5802
DrBennemin <benjamin.krzis@gmail.com>
2020-10-06 18:44:09 +0200

    Design search position

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -7,35 +7,68 @@ import SearchInput from "../components/SearchInput";
 function SearchResults() {
   const [query, setQuery] = useState("");
   const [resultItems, setResultItems] = useState([]);
+  const [resultRentals, setResultRentals] = useState([]);
 
   useEffect(() => {
-    async function fetchItems() {
+    async function fetchResults() {
       try {
         const loadedItems = await searchItem(query);
         const loadedRentals = await searchRent(query);
-        console.log(loadedRentals);
+        setResultRentals(loadedRentals);
         setResultItems(loadedItems);
       } catch (error) {
         console.log(error);
       }
     }
-    fetchItems();
+    fetchResults();
   }, [query]);
 
   return (
-    <Container>
-      <SearchInput query={query} onSetQuery={setQuery} />
-      <p>Sie suchen nach: {query}</p>
-      <Results>
-        {resultItems?.map((loadedItem) => (
-          <li key={loadedItem.id}>{loadedItem.title}</li>
-        ))}
-      </Results>
-    </Container>
+    <>
+      <Container>
+        <SearchInput query={query} onSetQuery={setQuery} />
+        <Results>
+          <ItemResults>
+            {resultItems?.map((loadedItem) => (
+              <li key={loadedItem.id}>{loadedItem.title}</li>
+            ))}
+          </ItemResults>
+
+          <RentResults>
+            {resultRentals?.map((loadedRentals) => (
+              <li key={loadedRentals.id}>
+                {loadedRentals.firstName} {loadedRentals.lastName}
+              </li>
+            ))}
+          </RentResults>
+        </Results>
+      </Container>
+    </>
   );
 }
 
-const Container = styled.div``;
-const Results = styled.ul``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Results = styled.div`
+  display: flex;
+  align-self: center;
+  width: 90%;
+  padding: 10px;
+  background-color: white;
+  height: 40%;
+  overflow: auto;
+  margin-top: -10px;
+`;
+const ItemResults = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
+const RentResults = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
 
 export default SearchResults;
