11b646efd3687f96eb2fee9bf8c1a7c578a3a8d9
DrBennemin <benjamin.krzis@gmail.com>
2020-10-09 14:17:50 +0200

    Link list and rent items

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -3,12 +3,16 @@ import React, { useEffect, useState } from "react";
 import { searchItem } from "../api/items";
 import { searchRent } from "../api/rentals";
 import SearchInput from "../components/SearchInput";
+import { Link, useParams } from "react-router-dom";
+import Menu from "../assets/menu.svg";
+import HeaderGoBack from "../components/HeaderGoBack";
 
 function SearchResults() {
   const [query, setQuery] = useState("");
   const [resultItems, setResultItems] = useState([]);
   const [resultRentals, setResultRentals] = useState([]);
   const [toggleSearch, setToggleSearch] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
     async function fetchResults() {
@@ -25,38 +29,61 @@ function SearchResults() {
   }, [query]);
 
   return (
-    <>
-      <Container>
-        <SearchInput
-          query={query}
-          onSetQuery={setQuery}
-          onSetToggleSearch={setToggleSearch}
-        />
-        {toggleSearch && (
-          <Results>
-            <ItemResults>
-              {resultItems?.map((loadedItem) => (
-                <li key={loadedItem.id}>{loadedItem.title}</li>
-              ))}
-            </ItemResults>
+    <Container>
+      {/* <HeaderGoBack
+        title={"Suche"}
+        left={
+          <Link to="/">
+            <img src={Menu} alt="menu-icon" />
+          </Link>
+        }
+      /> */}
 
-            <RentResults>
-              {resultRentals?.map((loadedRentals) => (
-                <li key={loadedRentals.id}>
+      <SearchInput
+        query={query}
+        onSetQuery={setQuery}
+        onSetToggleSearch={setToggleSearch}
+      />
+      {toggleSearch && (
+        <Results>
+          <ItemResults>
+            {resultItems?.map((loadedItem) => (
+              <Link
+                to={`/item/detail/${loadedItem.id}`}
+                key={loadedItem.id}
+                id={loadedItem.id}
+              >
+                <li>{loadedItem.title}</li>
+              </Link>
+            ))}
+          </ItemResults>
+
+          <RentResults>
+            {resultRentals?.map((loadedRentals) => (
+              <Link
+                to={`/rent/detail/${loadedRentals.id}`}
+                key={loadedRentals.id}
+                id={loadedRentals.id}
+              >
+                <li>
                   {loadedRentals.firstName} {loadedRentals.lastName}
                 </li>
-              ))}
-            </RentResults>
-          </Results>
-        )}
-      </Container>
-    </>
+              </Link>
+            ))}
+          </RentResults>
+        </Results>
+      )}
+    </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding-top: 1.4em;
+  margin-bottom: 4em;
+  margin-top: 4em;
+  width: 90%;
 `;
 
 const Results = styled.div`
